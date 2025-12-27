8b8a765dc76429f2873b1f5cb932d66db80107cd
Razi <razijemni264@gmail.com>
2025-12-27 14:28:28 +0100

    updated vercel AnalyticsClient

--- app/AnalyticsClient.tsx
+++ app/AnalyticsClient.tsx
@@ -3,6 +3,6 @@
 import { Analytics } from "@vercel/analytics/react"; // remove if not using Vercel analytics
 
 export default function AnalyticsClient() {
-  // If you don't use Vercel Analytics, replace the return with `return null;`
-  return <Analytics />;
+    // If you don't use Vercel Analytics, replace the return with `return null;`
+    return <Analytics />;
 }
